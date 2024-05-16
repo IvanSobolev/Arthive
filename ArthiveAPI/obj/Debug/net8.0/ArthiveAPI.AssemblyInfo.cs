289cd1fcddf20575611dedf6881ed4fbb43253aa
IvanSobolev <i@bezlikiy.ru>
2024-05-16 14:52:20 +0300

    Corsfix

--- ArthiveAPI/obj/Debug/net8.0/ArthiveAPI.AssemblyInfo.cs
+++ ArthiveAPI/obj/Debug/net8.0/ArthiveAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArthiveAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f853e9b56a1103e2ad24ade9226c2d32d8043e4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+934b6d101ea11cfb8a38c636914f60044df565fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArthiveAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArthiveAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
